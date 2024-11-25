04620980c8b70f80c22479ff0a12fb5d567fdcbb
iinshot <ernest_s@bk.ru>
2024-11-26 00:24:35 +0300

    Add files via upload

---
+++ Task20_KASD/task20/Program.cs
@@ -0,0 +1,47 @@
+﻿using HashMap;
+using System.Text.RegularExpressions;
+public class Definition
+{
+    enum TypeDef
+    {
+        Integer, 
+        Double, 
+        Float
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            MyHashMap<string, string> definition = new MyHashMap<string, string>();
+            string path = "input.txt";
+            string pattern = @"(double|int|float) \S* ?(?:=) ?(\S)+?(?=;)";
+            StreamReader sr = new StreamReader(path);
+            string? line = sr.ReadLine();
+            while (line != null)
+            {
+                MatchCollection matches = Regex.Matches(line, pattern);
+                foreach (Match match in matches)
+                {
+                    string[] part = match.Value.Split(' ');
+                    string type = part[0].Trim();
+                    string value = part[3].Trim();
+                    string name = part[1].Trim();
+                    string typeValue = type + " " + value;
+                    if (definition.ContainsKey(name))
+                        Console.WriteLine("Repeat" + " " + $"{type} {name} = {value}");
+                    else
+                        definition.Put(name, typeValue);
+                }
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            var pairs = definition.EntrySet();
+            foreach (var pair in pairs)
+                Console.WriteLine($"{pair.Value} : {pair.Key}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exeption: " + ex.Message);
+        }
+    }
+}
