1068ff22ed37785a6b4cfab57958ac868635a5e1
iinshot <ernest_s@bk.ru>
2024-11-26 00:23:05 +0300

    Add files via upload

---
+++ Task19_KASD/task19/Main.cs
@@ -0,0 +1,30 @@
+﻿using HashMap;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+public class Tegs
+{
+    static void Main(string[] args)
+    {
+        MyHashMap<string, int> tegs = new MyHashMap<string, int>();
+        string path = "input.txt";
+        StreamReader sr = new StreamReader(path);
+        string? line = sr.ReadLine();
+        string pattern = @"(?<=</?)?\w+ ?(?=/?>)";
+        while (line != null)
+        {
+            MatchCollection matches = Regex.Matches(line, pattern);
+            foreach (Match match in matches)
+            {
+                if (!tegs.ContainsKey(match.Value.ToLower()))
+                    tegs.Put(match.Value.ToLower(), 1);
+                else
+                    tegs.Put(match.Value.ToLower(), tegs.Get(match.Value.ToLower()) + 1);
+            }
+            line = sr.ReadLine();
+        }
+        sr.Close();
+        IEnumerable<KeyValuePair<string, int>> pairs = tegs.EntrySet();
+        foreach(var pair in pairs)
+            Console.WriteLine($"{pair.Key} : {pair.Value}");
+    }
+}
